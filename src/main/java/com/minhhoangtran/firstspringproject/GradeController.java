c2621902009f163144d07fe44c22201d0cc77e70
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-07-06 01:48:19 -0400

    update controller, refactor styles

--- src/main/java/com/minhhoangtran/firstspringproject/GradeController.java
+++ src/main/java/com/minhhoangtran/firstspringproject/GradeController.java
@@ -1,12 +1,23 @@
 package com.minhhoangtran.firstspringproject;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class GradeController {
+
+    List<Grade> studentGrades = new ArrayList<>();
+
     @GetMapping("/grades")
-    public String sayHello() {
+    public String sayHello(Model model) {
+        studentGrades.add(new Grade("Harry", "Potion", "C-"));
+        studentGrades.add(new Grade("Harry", "Potion", "C-"));
+        studentGrades.add(new Grade("Harry", "Potion", "C-"));
+        model.addAttribute("grade", studentGrades);
         return "grades";
     }
     
