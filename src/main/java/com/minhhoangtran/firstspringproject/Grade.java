c36337ef1a798bf41efe72dad15059c04b23842d
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-07-06 02:24:22 -0400

    update controller, form and table

--- src/main/java/com/minhhoangtran/firstspringproject/Grade.java
+++ src/main/java/com/minhhoangtran/firstspringproject/Grade.java
@@ -4,6 +4,7 @@ public class Grade {
     private String name;
     private String subject;
     private String score;
+
     public Grade() {
     }
 
@@ -36,5 +37,10 @@ public class Grade {
     public void setName(String name) {
         this.name = name;
     }
-    
+
+    @Override
+    public String toString() {
+        return "Grade [name=" + name + ", subject=" + subject + ", score=" + score + "]";
+    }
+
 }
--- src/main/java/com/minhhoangtran/firstspringproject/GradeController.java
+++ src/main/java/com/minhhoangtran/firstspringproject/GradeController.java
@@ -1,11 +1,13 @@
 package com.minhhoangtran.firstspringproject;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GradeController {
@@ -13,12 +15,22 @@ public class GradeController {
     List<Grade> studentGrades = new ArrayList<>();
 
     @GetMapping("/grades")
-    public String sayHello(Model model) {
-        studentGrades.add(new Grade("Harry", "Potion", "C-"));
-        studentGrades.add(new Grade("Harry", "Potion", "C-"));
-        studentGrades.add(new Grade("Harry", "Potion", "C-"));
-        model.addAttribute("grade", studentGrades);
+    public String getGrades(Model model) {
+        model.addAttribute("grades", studentGrades);
         return "grades";
     }
-    
+
+    @GetMapping(value = "/")
+    public String gradeForm(Model model) {
+        model.addAttribute("grade", new Grade());
+        return "form";
+    }
+
+    @PostMapping("/handleSubmit")
+    public String submitGrade(Grade grade) {
+        studentGrades.add(grade);
+        return "redirect:/grades";
+
+    }
+
 }
